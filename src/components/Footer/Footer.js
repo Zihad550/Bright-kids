e46b3d03b3cab7b2e395d8900b3d06e45af66650
Zihad550 <hussainmuzahid83@gmail.com>
2021-11-26 21:02:17 +0600

    footer mobile responsive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,17 +5,17 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="primary-title px-5 pt-5">
-      <div className="d-flex justify-content-evenly">
-        <div>
+      <div className="d-flex flex-column flex-md-row justify-content-evenly">
+        <div className="text-center text-md-start">
           <h4>Contact</h4>
-          <p className="text-start">
+          <p>
             329 Queensberry Street, North Melbourne <br />
             VIC 3051, Australia. <br />
             123 456 7890 <br />
             support@edumy.com
           </p>
         </div>
-        <div className="d-flex flex-column text-start">
+        <div className="d-flex flex-column text-center text-md-start my-4 my-md-0">
           <h4> COMPANY </h4>
           <a href="#" className="text-warning fs-5 text-decoration-none">
             About Us
@@ -30,7 +30,7 @@ const Footer = () => {
             Become a Teacher
           </a>
         </div>
-        <div className="d-flex flex-column text-start">
+        <div className="d-flex flex-column text-center text-md-start">
           <h4> SUPPORT </h4>
           <a href="#" className="text-warning fs-5 text-decoration-none">
             Documentation
@@ -48,7 +48,7 @@ const Footer = () => {
       </div>
 
       {/* copy right and social sextion */}
-      <div className="mt-4 d-flex justify-content-evenly align-items-center">
+      <div className="mt-4 d-flex flex-column-reverse flex-md-row justify-content-evenly align-items-center">
         <p className="m-0">&copy;2021 Bright Kids. All rights reserved. </p>
         <Social />
       </div>
